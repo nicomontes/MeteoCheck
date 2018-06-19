e99ffeaa4dfe01e5d08cfcc3e82b59155e09ec68
nmontes <nicolas.montes@telecom-sudparis.eu>
2018-06-19 11:27:25 +0200

    BIG UPDATE

--- meteocheck.rb
+++ meteocheck.rb
@@ -6,12 +6,16 @@ require 'json'
 require 'uri'
 require 'net/http'
 require 'net/https'
+require 'levenshtein'
+require 'damerau-levenshtein'
 
 # Write time and RUN
 puts Time.new.inspect
 puts "-------"
 puts "  Run  "
 puts "-------"
+
+puts DamerauLevenshtein.distance('qsd', 'sqkf')
 
 # Open Chrome
 $browser = Watir::Browser.new :chrome, :switches => %w[--disable-popup-blocking --disable-translate]
@@ -26,81 +30,48 @@ date = Date.today
 
 # Parse JSON file
 hash = JSON.parse(json_file)
-data = Hash.new
-
-forecast = Hash.new
-forecast['source'] = ''
-forecast['date_from'] = ''
-forecast['date_for'] = ''
-forecast['weather'] = ''
-forecast['t_min'] = ''
-forecast['t_max'] = ''
-
-city = Hash.new
-city['name'] = ''
-city['zip'] = ''
-city['forecast'] = [forecast]
-
-dataNew = Hash.new
-dataNew['cities'] = [city]
-
 
 i=0
 # Browse JSON file
 hash['cities'].each do |item|
 	puts "City => " + item["name"]
+	# Check Météo France
 	$browser.goto 'http://www.meteofrance.com/previsions-meteo-france/' + item['name'].to_s + '/' + item['zip'].to_s
 	j = 0
-	data[(date).to_s] = []
 	$browser.div(:class => 'liste-jours').ul.lis.each do |li|
-		data[(date).to_s][j] = {}
-		data[(date).to_s][j][(date+j).to_s] = {}
-		data[(date).to_s][j][(date+j).to_s]['MF'] = {}
-		data[(date).to_s][j][(date+j).to_s]['MF']['weather'] = li.title.to_s
-		data[(date).to_s][j][(date+j).to_s]['MF']['min_temp'] = /[\-0-9]*/.match(li.dl.dds[1].span(:class => 'min-temp').inner_html).to_s
-		data[(date).to_s][j][(date+j).to_s]['MF']['max_temp'] = /[\-0-9]*/.match(li.dl.dds[1].span(:class => 'max-temp').inner_html).to_s
+		forecast = { 
+			:source => 'Météo France', 
+			:date_from => (date).to_s, 
+			:date_for => (date+j).to_s,
+			:weather => li.title.to_s,
+			:t_min => /[\-0-9]*/.match(li.dl.dds[1].span(:class => 'min-temp').inner_html).to_s,
+			:t_max => /[\-0-9]*/.match(li.dl.dds[1].span(:class => 'max-temp').inner_html).to_s
+		}
+		hash['cities'][i]['forecast'].push(forecast)
 		j+=1
 	end
-	hash['cities'][i] = hash['cities'][i].merge(data)
-	#puts date.to_s + " : weather history"
-	k=0
-	while hash['cities'][i].key?((date-k).to_s)
-		#puts (date-k).to_s + " : " + hash['cities'][i][(date-k).to_s][k][(date).to_s]['weather'].to_s
-		k+=1
-	end
-
+	# Check Weather Channel
+	# Check Météo Agricole
+	
 	# Erase Files
-	l=0
-	while hash['cities'][i].key?((date-l).to_s)
-		m=0
-		system 'mkdir', '-p', item["name"]
-		hash['cities'][i][(date-l).to_s].each do |value|
-				File.open(item["name"]+'/'+item["name"]+'_'+(date-l+m).to_s, 'w') { |f|
-					f.write("date,weather,tempMax,tempMin\n")
+		system 'mkdir', '-p', hash['cities'][i]['name'].to_s
+		hash['cities'][i]['forecast'].each do |forecast|
+				File.open(hash['cities'][i]['name'].to_s+'/'+hash['cities'][i]['name'].to_s+'_'+forecast['date_for'].to_s, 'w') { |f|
+					f.write("date,source,weather,tempMax,tempMin\n")
 					f.close
 				}
-			#puts 'le ' + (date-l).to_s + ' Météo France à prévu pour le ' + (date-l+m).to_s + ' cette météo : ' + value[(date-l+m).to_s]['weather'].to_s
-			m+=1
 		end
-		l+=1
-	end
 
 	# Add in files
-	l=0
-	while hash['cities'][i].key?((date-l).to_s)
-		m=0
-		system 'mkdir', item["name"]
-		hash['cities'][i][(date-l).to_s].each do |value|
-			File.open(item["name"]+'/'+item["name"]+'_'+(date-l+m).to_s, 'a') { |f|
-		  	f.puts((date-l).to_s + ',' + value[(date-l+m).to_s]['MF']['weather'].to_s + ',' + value[(date-l+m).to_s]['MF']['max_temp'] + ',' + value[(date-l+m).to_s]['MF']['min_temp'])
+		system 'mkdir', hash['cities'][i]['name'].to_s
+		hash['cities'][i]['forecast'].each do |forecast|
+			File.open(hash['cities'][i]['name'].to_s+'/'+hash['cities'][i]['name'].to_s+'_'+forecast['date_for'].to_s, 'a') { |f|
+		  	f.puts(forecast['date_from'].to_s + ',' + forecast['source'].to_s + ',' + forecast['weather'].to_s + ',' + forecast['t_max'].to_s + ',' + forecast['t_min'].to_s)
 		  	f.close
 			}
-			#puts 'le ' + (date-l).to_s + ' Météo France à prévu pour le ' + (date-l+m).to_s + ' cette météo : ' + value[(date-l+m).to_s]['weather'].to_s
-			m+=1
 		end
-		l+=1
-	end
-	
+		
+		system 'rm', '-f', hash['cities'][i]['name'].to_s+'/'+hash['cities'][i]['name'].to_s+'_'
 	
 	i+=1
 end
@@ -112,11 +83,6 @@ $browser.close
 
 File.open('meteodata.json', 'w') { |f| 
   f.write(JSON.pretty_generate(hash))
-  f.close
-}
-
-File.open('meteodataNew.json', 'w') { |f| 
-  f.write(JSON.pretty_generate(dataNew))
   f.close
 }
 
